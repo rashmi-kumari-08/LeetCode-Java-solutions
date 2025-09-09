41f66f6bb202f5c6e75270e91d3e179bf4bc280c
rashmi-kumari-08 <rashmi.kumari.dev08@gmail.com>
2025-09-09 23:52:44 +0530

    169

--- 169. Majority Element.java
+++ 169. Majority Element.java
@@ -1,3 +1,5 @@
+
+//Optimal
 class Solution {
     public int majorityElement(int[] nums) {
 
@@ -16,5 +18,25 @@ class Solution {
     }
 
     return result;
+    }
+}
+// Brute force 
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+       HashMap<Integer,Integer> hashmap = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int count = hashmap.getOrDefault(nums[i],0);
+            hashmap.put(nums[i],count+1);
+        }
+        int max =0;
+        int result=0;
+        for(Map.Entry<Integer,Integer> m: hashmap.entrySet()){
+            if(m.getValue()> max){
+                max = m.getValue();
+                result = m.getKey();
+            }
+        }
+        return  result;
     }
 }
